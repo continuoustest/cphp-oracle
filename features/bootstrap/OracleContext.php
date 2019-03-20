4665d95d90774d109d292896f714bb869dcec5b3
Matthieu Nogatchewsky <matthieu.nogatchewsky@continuous.team>
2019-03-20 18:22:42 +0100

    Installing and configuring Behat

---
+++ features/bootstrap/OracleContext.php
@@ -0,0 +1,26 @@
+<?php
+
+use Behat\Behat\Context\Context;
+
+/**
+ * Defines Oracle server features from the specific context.
+ */
+class OracleContext implements Context 
+{
+   /**
+    * Initializes context.
+    */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @Then I could do a select
+     */
+    public function iCouldDoASelect()
+    {
+        
+    }
+
+}
+ 
